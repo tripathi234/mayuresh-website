9462584f03d6d4baea5140ce10b24f6b6cc4a983
tripathi234 <mayureshtripathi4@gmail.com>
2026-02-22 21:26:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,139 @@
+function adminLogin(){
+
+let pass = prompt("Enter Admin Password");
+
+if(pass === "mayuresh@123"){
+alert("Welcome Mayureshji 👑 Admin Access Granted");
+}
+else if(pass === null){
+alert("Login Cancelled");
+}
+else{
+alert("Wrong Password ❌");
+}
+
+}function adminLogin() {
+  
+  let pass = prompt("Enter Admin Password");
+  
+  if (pass === "mayuresh@123") {
+    alert("Welcome Mayureshji 👑 Admin Access Granted");
+  }
+  else if (pass === null) {
+    alert("Login Cancelled");
+  }
+  else {
+    alert("Wrong Password ❌");
+  }
+  
+}function adminLogin() {
+  
+  let pass = prompt("Enter Admin Password");
+  
+  if (pass === "mayuresh@123") {
+    alert("Welcome Mayureshji 👑 Admin Access Granted");
+  }
+  else if (pass === null) {
+    alert("Login Cancelled");
+  }
+  else {
+    alert("Wrong Password ❌");
+  }
+  
+}function adminLogin() {
+  
+  let pass = prompt("Enter Admin Password");
+  
+  if (pass === "mayuresh@123") {
+    alert("Welcome Mayureshji 👑 Admin Access Granted");
+  }
+  else if (pass === null) {
+    alert("Login Cancelled");
+  }
+  else {
+    alert("Wrong Password ❌");
+  }
+  
+}function adminLogin() {
+  
+  let pass = prompt("Enter Admin Password");
+  
+  if (pass === "mayuresh@123") {
+    alert("Welcome Mayureshji 👑 Admin Access Granted");
+  }
+  else if (pass === null) {
+    alert("Login Cancelled");
+  }
+  else {
+    alert("Wrong Password ❌");
+  }
+  
+}function adminLogin() {
+  
+  let pass = prompt("Enter Admin Password");
+  
+  if (pass === "mayuresh@123") {
+    alert("Welcome Mayureshji 👑 Admin Access Granted");
+  }
+  else if (pass === null) {
+    alert("Login Cancelled");
+  }
+  else {
+    alert("Wrong Password ❌");
+  }
+  
+}function adminLogin() {
+  
+  let pass = prompt("Enter Admin Password");
+  
+  if (pass === "mayuresh@123") {
+    alert("Welcome Mayureshji 👑 Admin Access Granted");
+  }
+  else if (pass === null) {
+    alert("Login Cancelled");
+  }
+  else {
+    alert("Wrong Password ❌");
+  }
+  
+}function adminLogin() {
+  
+  let pass = prompt("Enter Admin Password");
+  
+  if (pass === "mayuresh@123") {
+    alert("Welcome Mayureshji 👑 Admin Access Granted");
+  }
+  else if (pass === null) {
+    alert("Login Cancelled");
+  }
+  else {
+    alert("Wrong Password ❌");
+  }
+  
+}function adminLogin() {
+  
+  let pass = prompt("Enter Admin Password");
+  
+  if (pass === "mayuresh@123") {
+    alert("Welcome Mayureshji 👑 Admin Access Granted");
+  }
+  else if (pass === null) {
+    alert("Login Cancelled");
+  }
+  else {
+    alert("Wrong Password ❌");
+  }function adminLogin() {
+  
+  let pass = prompt("Enter Admin Password");
+  
+  if (pass === "mayuresh@123") {
+    alert("Welcome Mayureshji 👑 Admin Access Granted");
+  }
+  else if (pass === null) {
+    alert("Login Cancelled");
+  }
+  else {
+    alert("Wrong Password ❌");
+  }
+  
+}
